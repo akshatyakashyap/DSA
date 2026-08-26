3eabe243e8d6c38369f446e3acbc09adf99cf7f8
akshatyakashyap <akshatyak@gmail.com>
2026-08-26 22:25:08 +0530

    Time: 7 ms (18.55%) | Memory: 42.7 MB (63.72%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int stairs=0;
+
+            while(n>=stairs+1){
+                stairs++;
+                n -= stairs;
+            } return stairs;
+    }
+}
